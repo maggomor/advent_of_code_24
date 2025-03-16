5e5e33a8236caad579f33e7def899335d0bc02d9
MaggoMor <maggomorizon@googlemail.com>
2025-03-16 17:38:26 +0100

    Day One Complete

---
+++ python_code/src/day1/main.py
@@ -0,0 +1,34 @@
+import pydantic
+
+from pydantic import BaseModel
+import numpy as np
+
+class DayOneClass(BaseModel):
+    list_one: list
+    list_two: list
+    distance: float = None
+
+    def __post_init__(self, list_one: list, list_two: list, distance: float = None):
+        self.list_one = list_one
+        self.list_two = list_two
+        self.distance = distance
+
+    def check_list_correctness(self):
+        if len(self.list_one) != len(self.list_two):
+            raise ValueError("Lists are of different lengths")
+        for x, y in zip(self.list_one, self.list_two):
+            if type(x) not in [int, float] or type(y) not in [int, float]:
+                raise ValueError("Wrong Type, only nums allowed!")
+        return True
+            
+    def compare_lists(self):
+        self.check_list_correctness()
+        self.distance = np.sum(np.array(np.abs(np.array(self.list_one) - np.array(self.list_two))))
+        return self.distance
+    
+if __name__ == "__main__":
+    list_one = [1,5,6,4]
+    list_two = [2,3,4,9]
+    checker = DayOneClass(list_one=list_one, list_two=list_two)
+    outcome = checker.compare_lists()
+    print(f"Distance between lists {list_one} and {list_two} is: {outcome}")
---
+++ python_code/tests/dayone_test.py
@@ -0,0 +1,30 @@
+import pytest 
+
+from python_code.src.day1.main import DayOneClass
+
+class TestDayOneClass:
+    def test_check_list_correctness(self):
+        list_one = [1,5,6,4]
+        list_two = [2,3,4,9]
+        checker = DayOneClass(list_one=list_one, list_two=list_two)
+        assert checker.check_list_correctness() == True
+
+    def test_check_list_correctness_error(self):
+        list_one = [1,5,6,4]
+        list_two = [2,3,4,9, 10]
+        checker = DayOneClass(list_one=list_one, list_two=list_two)
+        with pytest.raises(ValueError):
+            checker.check_list_correctness()
+
+    def test_check_list_correctness_error_type(self):
+        list_one = [1,5,6,4]
+        list_two = [2,3,4,"a"]
+        checker = DayOneClass(list_one=list_one, list_two=list_two)
+        with pytest.raises(ValueError):
+            checker.check_list_correctness()
+        
+    def test_compare_lists(self):
+        list_one = [1,5,6,4]
+        list_two = [2,3,4,9]
+        checker = DayOneClass(list_one=list_one, list_two=list_two)
+        assert checker.compare_lists() == 10
