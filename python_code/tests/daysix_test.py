26685045328d76bd951defb23801ec6d60564eaf
maggomor <117517998+maggomor@users.noreply.github.com>
2025-03-20 15:33:50 +0100

    Day six branch (#4)
    * Added day 6
    
    * Finalized Day Six.
    
    * Finalized.
    
    ---------
    
    Co-authored-by: MaggoMor <maggomorizon@googlemail.com>

---
+++ python_code/tests/daysix_test.py
@@ -0,0 +1,36 @@
+import pytest 
+import numpy as np
+
+from python_code.src.day6.main import DaySixClass
+
+
+class TestDaySixClass:
+    def test_check_connections(self):
+        checker = DaySixClass()
+        input_maze = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#..."
+        maze_visited, visited, length = checker.run_through_maze(input_maze = input_maze)
+        assert length == 41
+
+    def test_check_raise_Value_Error_Wrong_Input_Letter(self):
+        checker = DaySixClass()
+        input_maze = "....#.....\n.........A\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#..."
+        with pytest.raises(ValueError):
+            maze_visited, visited, length = checker.run_through_maze(input_maze = input_maze)
+        
+    def test_check_raise_Value_Error_Wrong_Input_Dimensions(self):
+        checker = DaySixClass()
+        input_maze = "....#.....\n.........\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#..."
+        with pytest.raises(ValueError):
+            maze_visited, visited, length = checker.run_through_maze(input_maze = input_maze)
+
+    def test_check_raise_Value_Error_Wrong_No_Player(self):
+        checker = DaySixClass()
+        input_maze = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#........\n........#.\n#.........\n......#..."
+        with pytest.raises(ValueError):
+            maze_visited, visited, length = checker.run_through_maze(input_maze = input_maze)
+
+    def test_check_raise_Value_Error_Infinite_Loop(self):
+        checker = DaySixClass()
+        input_maze = "....#.....\n.........#\n..........\n..#.......\n.......#..\n....#.....\n.#.#^#....\n....#...#.\n#.........\n......#..."
+        with pytest.raises(ValueError):
+            maze_visited, visited, length = checker.run_through_maze(input_maze = input_maze)
