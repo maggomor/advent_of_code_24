d2119f3e0f9207adad25b376df0c9725eceae7e8
MaggoMor <maggomorizon@googlemail.com>
2025-04-01 19:13:00 +0200

    Added Day 9.

---
+++ python_code/tests/daynine_test.py
@@ -0,0 +1,21 @@
+import pytest 
+
+from python_code.src.day9.main import DayNineClass
+
+class TestDayOneClass:
+    def test_check_list_correctness(self):
+        data = "2333133121414131402"
+        checker = DayNineClass()
+        assert checker.run_program(data) == 1928
+
+    def test_check_assertion_error(self):
+        data = "2A33133121414131402"
+        checker = DayNineClass()
+        with pytest.raises(ValueError):
+            checker.run_program(data)
+
+    def test_check_assertion_error_dot(self):
+        data = "2A331.3121414131402"
+        checker = DayNineClass()
+        with pytest.raises(ValueError):
+            checker.run_program(data)
